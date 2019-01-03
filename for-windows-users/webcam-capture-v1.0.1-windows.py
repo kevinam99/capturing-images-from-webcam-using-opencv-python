c17a3b7404b9b8a57614c21d2f9e845b182f8593
Kevin A Mathew <30661278+kevinam99@users.noreply.github.com>
2019-01-03 20:57:20 +0530

    Add files via upload

---
+++ for-windows-users/webcam-capture-v1.0.1-windows.py
@@ -0,0 +1,31 @@
+import cv2
+from time import sleep
+key = cv2. waitKey(1)
+webcam = cv2.VideoCapture(0)
+sleep(2)
+while True:
+     
+    check, frame = webcam.read()
+    print(check) #prints true as long as the webcam is running
+    print(frame) #prints matrix values of each framecd 
+    cv2.imshow("Capturing", frame)
+    key = cv2.waitKey(1)
+    if key == ord('s'): 
+        cv2.imwrite(filename='saved_img.jpg', img=frame)
+        webcam.release()
+        print("Processing image...")
+        img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
+        print("Converting RGB image to grayscale...")
+        gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+        print("Converted RGB image to grayscale...")
+        print("Resizing image to 28x28 scale...")
+        img_ = cv2.resize(gray,(28,28))
+        print("Resized...")
+        img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
+        print("Image saved!")
+         
+        break
+    elif key == ord('q'):
+        webcam.release()
+        cv2.destroyAllWindows()
+        break
