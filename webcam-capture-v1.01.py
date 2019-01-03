c17ea9849991625aa764223d51d38dcd57b6f3fb
Kevin A Mathew <30661278+kevinam99@users.noreply.github.com>
2019-01-03 20:43:23 +0530

    Update webcam-capture-v1.01.py

--- webcam-capture-v1.01.py
+++ webcam-capture-v1.01.py
@@ -18,8 +18,12 @@ while True:
         cv2.destroyAllWindows()
         print("Processing image...")
         img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
+        print("Converting RGB image to grayscale...")
         gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+        print("Converted RGB image to grayscale...")
+        print("Resizing image to 28x28 scale...")
         img_ = cv2.resize(gray,(28,28))
+        print("Resized...")
         img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
         print("Image saved!")
      
