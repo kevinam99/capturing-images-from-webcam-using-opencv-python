4dc5e47ce584033d3d7366cc126f317f304c20f4
Kevin A Mathew <30661278+kevinam99@users.noreply.github.com>
2019-01-03 20:15:09 +0530

    Update webcam-capture-v1.01.py

--- webcam-capture-v1.01.py
+++ webcam-capture-v1.01.py
@@ -11,17 +11,21 @@ while True:
     key = cv2.waitKey(1)
     if key == ord('s'): 
         cv2.imwrite(filename='saved_img.jpg', img=frame)
-         
+        webcam.release()
+        img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
+        img_new = cv2.imshow("Captured Image", img_new)
+        cv2.waitKey(1650)
+        cv2.destroyAllWindows()
+        img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
+        gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+        img_ = cv2.resize(gray,(28,28))
+        img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
+
         break
     elif key == ord('q'):
+        print("Turning off camera.")
+        webcam.release()
+        print("Camera off.")
+        print("Program ended.")
+        cv2.destroyAllWindows()
         break
-webcam.release()
-img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
-img_new = cv2.imshow("Captured Image", img_new)
-cv2.waitKey(1650)
-cv2.destroyAllWindows()
-
-img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
-gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-img_ = cv2.resize(gray,(28,28))
-img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
