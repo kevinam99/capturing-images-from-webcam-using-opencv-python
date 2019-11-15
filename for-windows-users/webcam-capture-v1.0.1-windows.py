79a79b3e5eac54dcd5a8030d1a3bccca14b3c8ec
kevinam99 <kevinam99@gmail.com>
2019-11-15 23:14:26 +0530

    Handled KeyboardInterrupt exception.

--- for-windows-users/webcam-capture-v1.0.1-windows.py
+++ for-windows-users/webcam-capture-v1.0.1-windows.py
@@ -4,28 +4,38 @@ key = cv2. waitKey(1)
 webcam = cv2.VideoCapture(0)
 sleep(2)
 while True:
-     
-    check, frame = webcam.read()
-    print(check) #prints true as long as the webcam is running
-    print(frame) #prints matrix values of each framecd 
-    cv2.imshow("Capturing", frame)
-    key = cv2.waitKey(1)
-    if key == ord('s'): 
-        cv2.imwrite(filename='saved_img.jpg', img=frame)
+
+    try:
+        check, frame = webcam.read()
+        print(check) #prints true as long as the webcam is running
+        print(frame) #prints matrix values of each framecd 
+        cv2.imshow("Capturing", frame)
+        key = cv2.waitKey(1)
+        if key == ord('s'): 
+            cv2.imwrite(filename='saved_img.jpg', img=frame)
+            webcam.release()
+            print("Processing image...")
+            img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
+            print("Converting RGB image to grayscale...")
+            gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+            print("Converted RGB image to grayscale...")
+            print("Resizing image to 28x28 scale...")
+            img_ = cv2.resize(gray,(28,28))
+            print("Resized...")
+            img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
+            print("Image saved!")
+            
+            break
+        
+        elif key == ord('q'):
+            webcam.release()
+            cv2.destroyAllWindows()
+            break
+    
+    except(KeyboardInterrupt):
+        print("Turning off camera.")
         webcam.release()
-        print("Processing image...")
-        img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
-        print("Converting RGB image to grayscale...")
-        gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-        print("Converted RGB image to grayscale...")
-        print("Resizing image to 28x28 scale...")
-        img_ = cv2.resize(gray,(28,28))
-        print("Resized...")
-        img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
-        print("Image saved!")
-         
-        break
-    elif key == ord('q'):
-        webcam.release()
+        print("Camera off.")
+        print("Program ended.")
         cv2.destroyAllWindows()
         break
--- webcam-capture-v1.01.py
+++ webcam-capture-v1.01.py
@@ -3,35 +3,44 @@ import cv2
 key = cv2. waitKey(1)
 webcam = cv2.VideoCapture(0)
 while True:
-     
-    check, frame = webcam.read()
-    print(check) #prints true as long as the webcam is running
-    print(frame) #prints matrix values of each framecd 
-    cv2.imshow("Capturing", frame)
-    key = cv2.waitKey(1)
-    if key == ord('s'): 
-        cv2.imwrite(filename='saved_img.jpg', img=frame)
-        webcam.release()
-        img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
-        img_new = cv2.imshow("Captured Image", img_new)
-        cv2.waitKey(1650)
-        cv2.destroyAllWindows()
-        print("Processing image...")
-        img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
-        print("Converting RGB image to grayscale...")
-        gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-        print("Converted RGB image to grayscale...")
-        print("Resizing image to 28x28 scale...")
-        img_ = cv2.resize(gray,(28,28))
-        print("Resized...")
-        img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
-        print("Image saved!")
-     
-        break
-    elif key == ord('q'):
+    try:
+        check, frame = webcam.read()
+        print(check) #prints true as long as the webcam is running
+        print(frame) #prints matrix values of each framecd 
+        cv2.imshow("Capturing", frame)
+        key = cv2.waitKey(1)
+        if key == ord('s'): 
+            cv2.imwrite(filename='saved_img.jpg', img=frame)
+            webcam.release()
+            img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
+            img_new = cv2.imshow("Captured Image", img_new)
+            cv2.waitKey(1650)
+            cv2.destroyAllWindows()
+            print("Processing image...")
+            img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
+            print("Converting RGB image to grayscale...")
+            gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+            print("Converted RGB image to grayscale...")
+            print("Resizing image to 28x28 scale...")
+            img_ = cv2.resize(gray,(28,28))
+            print("Resized...")
+            img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
+            print("Image saved!")
+        
+            break
+        elif key == ord('q'):
+            print("Turning off camera.")
+            webcam.release()
+            print("Camera off.")
+            print("Program ended.")
+            cv2.destroyAllWindows()
+            break
+        
+    except(KeyboardInterrupt):
         print("Turning off camera.")
         webcam.release()
         print("Camera off.")
         print("Program ended.")
         cv2.destroyAllWindows()
         break
+    
